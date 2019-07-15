3bab747d3e33bc3e59edd4f0b7b34b5bbbb557ae
James Adarich <jadarich@neworbit.co.uk>
2019-07-15 22:49:15 +0100

    remove redundant tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,15 +5,11 @@ import { loadPolyfills, FetchPolyfill } from '.';
 export class CorrectIndexExports {
     @Test('loadPolyfills function is exported')
     public loadPolyfillsExported() {
-        Expect(loadPolyfills).toBeDefined();
-        Expect(loadPolyfills).not.toBeNull();
         Expect(loadPolyfills).toEqual(Any(Function));
     }
 
     @Test('FetchPolyfill is exported')
     public fetchPolyfillExported() {
-        Expect(FetchPolyfill).toBeDefined();
-        Expect(FetchPolyfill).not.toBeNull();
         Expect(FetchPolyfill).toEqual(Any(Object));
     }
 }
