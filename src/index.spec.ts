ae76f45ad21e312dbbe9ca7d4baca0e79cf10eec
James Adarich <jadarich@neworbit.co.uk>
2019-07-18 13:50:21 +0100

    add tests for promise polyfill

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,25 @@
 import { Any, Expect, Test, TestFixture } from 'alsatian';
-import { loadPolyfills, FetchPolyfill } from '.';
+import mock from 'mock-require';
+
+// currently mocking due to es modules not supported in node where we run the tests
+mock('promise-polyfill/src/polyfill', './promise.mock');
 
 @TestFixture('correct index exports')
 export class CorrectIndexExports {
     @Test('loadPolyfills function is exported')
-    public loadPolyfillsExported() {
+    public async loadPolyfillsExported() {
+        const { loadPolyfills } = await import('.');
         Expect(loadPolyfills).toEqual(Any(Function));
     }
 
     @Test('FetchPolyfill is exported')
-    public fetchPolyfillExported() {
+    public async fetchPolyfillExported() {
+        const { FetchPolyfill } = await import('.');
         Expect(FetchPolyfill).toEqual(Any(Object));
+    }
+
+    @Test('Promise polyfill is applied')
+    public async promisePolyfillIsApplied() {
+        Expect((global as NodeJS.Global & { PromisePolyfilled: boolean }).PromisePolyfilled).toBe(true);
     }
 }
